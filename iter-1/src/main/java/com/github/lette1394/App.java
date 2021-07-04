c54c6f475d613e13ed9e99ae1036bb7f5ab9a8f9
Gyoung-min <78856207+Gyoung-min@users.noreply.github.com>
2021-07-04 22:17:16 +0900

    요구 사항1 구현 (#2)
    * feat: 요구 사항1 2+3=5 구현
    
    - 최소 구현만 진행
    - 이후 요구사항 진행 시 재구현 필요
    
    * refactor: final 추가

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -1,7 +1,11 @@
 package com.github.lette1394;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    final Scanner sc = new Scanner(System.in);
+    sc.nextLine();
+    System.out.println(5);
   }
 }
