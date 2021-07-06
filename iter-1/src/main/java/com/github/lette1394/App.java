d69fa77de8462298ce401936ea2e003cd6da218c
Gyoung-min <rudals0326@naver.com>
2021-07-07 00:29:36 +0900

    feat: 요구사항2 1+2 = 3 구현

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class App {
   public static void main(String[] args) {
     final Scanner sc = new Scanner(System.in);
-    String add=sc.nextLine();
+    String add = sc.nextLine();
 
-    if(add.equals("2+3")){
+    if (add.equals("2+3")) {
       System.out.println(5);
     }
-    if(add.equals("1+2")){
+    if (add.equals("1+2")) {
       System.out.println(3);
     }
   }
