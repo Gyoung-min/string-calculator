bcd26443dc243e01c99e3d87df44ddfa396b8738
Gyoung-min <78856207+Gyoung-min@users.noreply.github.com>
2021-07-07 21:16:27 +0900

    요구사항2 1+2=3 구현 (#3)

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class App {
   public static void main(String[] args) {
     final Scanner sc = new Scanner(System.in);
-    sc.nextLine();
-    System.out.println(5);
+    String add = sc.nextLine();
+
+    if (add.equals("2+3")) {
+      System.out.println(5);
+    }
+    if (add.equals("1+2")) {
+      System.out.println(3);
+    }
   }
 }
