5e75578e9dcf731a5215d691ea06c7caabd485ec
Gyoung-min <rudals0326@naver.com>
2021-07-09 02:59:43 +0900

    feat: 요구사항 3 N+M 구현

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -1,9 +1,13 @@
 package com.github.lette1394;
 
+
+
 import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
+    final long N = 10;
+    final long M = 89;
     final Scanner sc = new Scanner(System.in);
     String add = sc.nextLine();
 
@@ -12,6 +16,9 @@ public class App {
     }
     if (add.equals("1+2")) {
       System.out.println(3);
+    }
+    if(add.equals("N+M")){
+      System.out.println(N+M);
     }
   }
 }
