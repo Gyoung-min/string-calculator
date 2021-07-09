97b388cc6d1033a7f896ec021d3b13bc07e61665
Gyoung-min <78856207+Gyoung-min@users.noreply.github.com>
2021-07-09 22:44:44 +0900

    요구사항3 N+M 구현 (#4)

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -1,17 +1,22 @@
 package com.github.lette1394;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
-  public static void main(String[] args) {
-    final Scanner sc = new Scanner(System.in);
-    String add = sc.nextLine();
 
-    if (add.equals("2+3")) {
-      System.out.println(5);
-    }
-    if (add.equals("1+2")) {
-      System.out.println(3);
-    }
+  public static void main(String[] args) {
+
+    final Scanner sc = new Scanner(System.in);
+    final String add = sc.nextLine();
+
+    final List<String> expression = new ArrayList<>(Arrays.asList(add.split("\\+")));
+    final long leftOperand = Long.parseLong(expression.get(0));
+    final long rightOperand = Long.parseLong(expression.get(1));
+    final String sum = String.valueOf(leftOperand + rightOperand);
+
+    System.out.println(sum);
   }
 }
