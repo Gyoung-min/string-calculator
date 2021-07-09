dcf1a9a91b7328711734b461650f9496504aee53
Gyoung-min <rudals0326@naver.com>
2021-07-09 21:48:26 +0900

    fix : 요구사항 3 수정
    - 배열을 리스트로 수정
    - 변수 선언과 동시에 초기화

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -1,22 +1,21 @@
 package com.github.lette1394;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
-  public static void main(String[] args) {
-    final String[] expression;
-    final long N;
-    final long M;
-    final String sum;
-    final Scanner sc = new Scanner(System.in);
 
+  public static void main(String[] args) {
+
+    final Scanner sc = new Scanner(System.in);
     String add = sc.nextLine();
 
-    expression = add.split("\\+");
-    N = Long.parseLong(expression[0]);
-    M = Long.parseLong(expression[1]);
-    sum = String.valueOf(N + M);
+    final List<String> expression = new ArrayList<>(Arrays.asList(add.split("\\+")));
+    final long N = Long.parseLong(expression.get(0));
+    final long M = Long.parseLong(expression.get(1));
+    final String sum = String.valueOf(N + M);
 
     System.out.println(sum);
   }
