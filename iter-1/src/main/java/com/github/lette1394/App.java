b2e8d670bf88d65fbea5248913a4843e26d4fe65
Gyoung-min <rudals0326@naver.com>
2021-07-09 22:38:06 +0900

    fix : 요구사항 3 추가 및 수정
    - String add에서 final String add로 수정
    - 변수명 N,M을 각각 leftOperand,rightOperand로 수정

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -10,12 +10,12 @@ public class App {
   public static void main(String[] args) {
 
     final Scanner sc = new Scanner(System.in);
-    String add = sc.nextLine();
+    final String add = sc.nextLine();
 
     final List<String> expression = new ArrayList<>(Arrays.asList(add.split("\\+")));
-    final long N = Long.parseLong(expression.get(0));
-    final long M = Long.parseLong(expression.get(1));
-    final String sum = String.valueOf(N + M);
+    final long leftOperand = Long.parseLong(expression.get(0));
+    final long rightOperand = Long.parseLong(expression.get(1));
+    final String sum = String.valueOf(leftOperand + rightOperand);
 
     System.out.println(sum);
   }
