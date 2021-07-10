2f687a856f854d0b7bd9c994e37cad70067f1bb2
Gyoung-min <rudals0326@naver.com>
2021-07-10 23:06:44 +0900

    fix : 포맷팅

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -16,12 +16,12 @@ public class App {
     final long operand1 = Long.parseLong(expression.get(0));
     final long operand2 = Long.parseLong(expression.get(1));
     final long operand3 = Long.parseLong(expression.get(2));
-    if(expression.size()==2) {
+    if (expression.size() == 2) {
       final String sum = String.valueOf(operand1 + operand2);
       System.out.println(sum);
     }
-    if(expression.size()==3){
-      final String sum = String.valueOf(operand1 + operand2+operand3);
+    if (expression.size() == 3) {
+      final String sum = String.valueOf(operand1 + operand2 + operand3);
       System.out.println(sum);
     }
 
