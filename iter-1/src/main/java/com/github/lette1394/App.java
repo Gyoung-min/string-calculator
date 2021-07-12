24ec4c3d1c4b76a036311bb0fc32955b2026f807
Gyoung-min <rudals0326@naver.com>
2021-07-12 23:08:10 +0900

    feat : 요구사항 5 연속 덧샘 구현

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -1,11 +1,27 @@
 package com.github.lette1394;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
     final Scanner sc = new Scanner(System.in);
-    sc.nextLine();
-    System.out.println(5);
+    final String getOperator = sc.nextLine();
+
+    final List<String> expression = new ArrayList<>(Arrays.asList(getOperator.split("\\+")));
+
+
+    long sum = 0;
+
+    for (int i = 0; i < expression.size(); i++) {
+
+      sum += Long.parseLong(expression.get(i));
+
+    }
+
+    System.out.println(sum);
   }
 }
