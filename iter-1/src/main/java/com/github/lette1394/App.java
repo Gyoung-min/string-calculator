8689569939526070f4295dc7de3c1f850e3ffcc8
Gyoung-min <rudals0326@naver.com>
2021-07-12 15:48:22 +0900

    fix : 빈 줄 추가

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -16,6 +16,7 @@ public class App {
     final long operand1 = Long.parseLong(expression.get(0));
     final long operand2 = Long.parseLong(expression.get(1));
     final long operand3 = Long.parseLong(expression.get(2));
+
     if (expression.size() == 2) {
       final String sum = String.valueOf(operand1 + operand2);
       System.out.println(sum);
