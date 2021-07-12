c588a6cf9baa92457a7097389e8c6f0a36282856
Gyoung-min <78856207+Gyoung-min@users.noreply.github.com>
2021-07-12 18:16:43 +0900

    feat : 요구사항 4 N+M+K 구현 (#7)

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -10,6 +10,23 @@ public class App {
   public static void main(String[] args) {
 
     final Scanner sc = new Scanner(System.in);
+
+    final String getOperator = sc.nextLine();
+
+    final List<String> expression = new ArrayList<>(Arrays.asList(getOperator.split("\\+")));
+    final long operand1 = Long.parseLong(expression.get(0));
+    final long operand2 = Long.parseLong(expression.get(1));
+    final long operand3 = Long.parseLong(expression.get(2));
+
+    if (expression.size() == 2) {
+      final String sum = String.valueOf(operand1 + operand2);
+      System.out.println(sum);
+    }
+    if (expression.size() == 3) {
+      final String sum = String.valueOf(operand1 + operand2 + operand3);
+      System.out.println(sum);
+    }
+
     final String add = sc.nextLine();
 
     final List<String> expression = new ArrayList<>(Arrays.asList(add.split("\\+")));
@@ -18,5 +35,6 @@ public class App {
     final String sum = String.valueOf(leftOperand + rightOperand);
 
     System.out.println(sum);
+
   }
 }
