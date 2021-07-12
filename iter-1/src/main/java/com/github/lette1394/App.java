250d85120d35698337b6c3051285d5a94ff2a99f
Gyoung-min <rudals0326@naver.com>
2021-07-12 21:15:23 +0900

    fix : 컴파일 오류 수정

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -23,18 +23,8 @@ public class App {
       System.out.println(sum);
     }
     if (expression.size() == 3) {
-      final String sum = String.valueOf(operand1 + operand2 + operand3);
+      final String sum = String.valueOf(operand1+operand2 + operand3);
       System.out.println(sum);
     }
-
-    final String add = sc.nextLine();
-
-    final List<String> expression = new ArrayList<>(Arrays.asList(add.split("\\+")));
-    final long leftOperand = Long.parseLong(expression.get(0));
-    final long rightOperand = Long.parseLong(expression.get(1));
-    final String sum = String.valueOf(leftOperand + rightOperand);
-
-    System.out.println(sum);
-
   }
 }
