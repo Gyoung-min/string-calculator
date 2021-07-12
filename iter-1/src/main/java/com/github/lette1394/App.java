02e1e77fa2a681cbcac3bc0826e15c39483fcbe4
Gyoung-min <78856207+Gyoung-min@users.noreply.github.com>
2021-07-12 22:44:58 +0900

    Requirements/4 fix (#10)

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -16,25 +16,16 @@ public class App {
     final List<String> expression = new ArrayList<>(Arrays.asList(getOperator.split("\\+")));
     final long operand1 = Long.parseLong(expression.get(0));
     final long operand2 = Long.parseLong(expression.get(1));
-    final long operand3 = Long.parseLong(expression.get(2));
+
 
     if (expression.size() == 2) {
       final String sum = String.valueOf(operand1 + operand2);
       System.out.println(sum);
     }
     if (expression.size() == 3) {
-      final String sum = String.valueOf(operand1 + operand2 + operand3);
+      final long operand3 = Long.parseLong(expression.get(2));
+      final String sum = String.valueOf(operand1+operand2 + operand3);
       System.out.println(sum);
     }
-
-    final String add = sc.nextLine();
-
-    final List<String> expression = new ArrayList<>(Arrays.asList(add.split("\\+")));
-    final long leftOperand = Long.parseLong(expression.get(0));
-    final long rightOperand = Long.parseLong(expression.get(1));
-    final String sum = String.valueOf(leftOperand + rightOperand);
-
-    System.out.println(sum);
-
   }
 }
