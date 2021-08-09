1b35c70e5a519d35e84bb645e2e4e005d326eaca
Gyoung-min <rudals0326@naver.com>
2021-08-10 04:27:24 +0900

    feat : 요구사항 6 로마 덧셈 구현

--- iter-1/src/main/java/com/github/lette1394/App.java
+++ iter-1/src/main/java/com/github/lette1394/App.java
@@ -9,21 +9,84 @@ public class App {
 
   public static void main(String[] args) {
 
+
+    long sum = 0;
+
     final Scanner sc = new Scanner(System.in);
 
     final String getOperator = sc.nextLine();
 
-    final List<String> expression = new ArrayList<>(Arrays.asList(getOperator.split("\\+")));
+    if (getOperator.contains("I") || getOperator.contains("V") || getOperator.contains("X") || getOperator.contains("L") || getOperator.contains("C") || getOperator.contains("D") || getOperator.contains("M")) {
 
+      romanNumberOperate(getOperator);
+
+    } else {
+
+      final List<String> expression = new ArrayList<>(Arrays.asList(getOperator.split("\\+")));
+
+
+      for (int i = 0; i < expression.size(); i++) {
+
+
+        sum += Long.parseLong(expression.get(i));
+
+      }
+
+
+      System.out.println(sum);
+    }
+  }
+
+  private static void romanNumberOperate(String getOperator) {
 
     long sum = 0;
 
+
+    final List<String> expression = new ArrayList<>(Arrays.asList(getOperator.split("\\+")));
+
     for (int i = 0; i < expression.size(); i++) {
 
-      sum += Long.parseLong(expression.get(i));
+      if (expression.get(i).equals("I")) {
+
+        sum += 1L;
+
+      }
+      if (expression.get(i).equals("V")) {
+
+        sum += 5L;
+
+      }
+      if (expression.get(i).equals("X")) {
+
+        sum += 10L;
+
+      }
+      if (expression.get(i).equals("L")) {
+
+        sum += 50L;
+
+      }
+      if (expression.get(i).equals("C")) {
+
+        sum += 100L;
+
+      }
+      if (expression.get(i).equals("D")) {
+
+        sum += 500L;
+
+      }
+      if (expression.get(i).equals("M")) {
+
+        sum += 1000L;
+
+      }
 
     }
-
     System.out.println(sum);
+
   }
+
+
 }
+
